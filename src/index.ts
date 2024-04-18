83fd99f5ccfcdb69b19ce211b6f90c22163a7d72
WhatACotton <whatacotton@gmail.com>
2024-04-18 22:44:28 +0900

    fix

--- src/index.ts
+++ src/index.ts
@@ -174,7 +174,7 @@ try {
 		const pagesDeployment = await createPagesDeployment();
 
 		let alias = "";
-		console.log(branch, production_branch);
+		console.log("branch:", branch, "prod_branch:", production_branch);
 
 		if (branch != production_branch) {
 			alias = generateURL(branch, pagesDeployment.url);
